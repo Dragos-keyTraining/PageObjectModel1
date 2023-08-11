34b5f4914c0ac90dc2f5285031e0c2db87b3dc7a
Dragos Tanta <dragos.tanta@gmail.com>
2023-08-11 18:12:19 +0300

    curs21

--- src/main/java/tests/XpathExample3.java
+++ src/main/java/tests/XpathExample3.java
@@ -27,6 +27,16 @@ public class XpathExample3 extends BaseTest{
 		
 		////*[self::span or self::a][contains(@class, 'page-numbers') and not(contains(text(), '1') or contains(text(), '2'))]
 		
+		
+		//atunci cand vreau ultimul element dar nu pot sa ma bazez pe index folosim last()
+		//--> (//h2[@class='woocommerce-loop-product__title'])[last()]  --> ultimul
+		//--> (//h2[@class='woocommerce-loop-product__title'])[last()-1] --> pen ultimul
+		
+		//similar cu index este si position()
+		//--> (//h2[@class='woocommerce-loop-product__title'])[position()=13]
+		
+		
+		
 	}
 
 }
